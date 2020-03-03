6cfd9d2f211c8465f5ef3fd025b3654a0dc46a55
Revadike <revadike@outlook.com>
2020-03-03 19:14:55 +0100

    Localized metadata

--- auto-task.user.js
+++ auto-task.user.js
@@ -1,53 +1,55 @@
 // ==UserScript==
-// @name         自动任务
-// @namespace    auto-task
-// @version      2.1.17
-// @description  自动完成赠key站任务
-// @author       HCLonely
-// @license      MIT
-// @iconURL      https://github.com/HCLonely/auto-task/raw/master/favicon.ico
-// @homepage     https://blog.hclonely.com/posts/777c60d5/
-// @supportURL   https://github.com/HCLonely/auto-task/issues/new/choose
-// @updateURL    https://github.com/HCLonely/auto-task/raw/master/auto-task.user.js
-// @include      *://giveaway.su/giveaway/view/*
-// @include      *://marvelousga.com/*
-// @include      *://dupedornot.com/*
-// @include      *://www.grabfreegame.com/giveaway/*
-// @include      *://www.bananagiveaway.com/giveaway/*
-// @include      *://gamecode.win/*
-// @include      /https?:\/\/gamehag.com/.*?giveaway\/.*/
-// @include      *://prys.revadike.com/giveaway/?id=*
-// @include      *://www.indiedb.com/giveaways*
-// @include      *://www.opiumpulses.com/giveaways
-// @include      *://gkey.fun/distribution/*
-// @include      *://givekey.ru/distribution/*
-// @include      *://takekey.ru/distribution/*
-// @include      *://chubkeys.com/giveaway/*
-// @include      *://giveawayhopper.com/giveaway/*
-// @include      *://*freegamelottery.com*
-// @include      *://gleam.io/*
-// @include      *://www.spoune.com/index.php*
-// @exclude      *googleads*
-// @include      https://blog.hclonely.com/auto-task/setting.html
-// @include      https://blog.hclonely.com/auto-task/announcement.html
-// @require      https://cdn.bootcss.com/vue/2.6.10/vue.min.js
-// @require      https://cdn.bootcss.com/element-ui/2.12.0/index.js
-// @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
-// @resource     css https://github.com/HCLonely/auto-task/raw/master/auto-task.min.css?ver=2.1.17
-// @grant        GM_setValue
-// @grant        GM_getValue
-// @grant        GM_listValues
-// @grant        GM_deleteValue
-// @grant        GM_addStyle
-// @grant        GM_xmlhttpRequest
-// @grant        GM_getResourceText
-// @grant        GM_registerMenuCommand
-// @grant        GM_info
-// @grant        GM_openInTab
-// @grant        GM_download
-// @connect      *
-// @run-at       document-end
-// @compatible   chrome >=58,没有测试其他浏览器的兼容性
+// @name           自动任务
+// @name:en        Auto Task
+// @namespace      auto-task
+// @version        2.1.17
+// @description    自动完成赠key站任务
+// @description:en Automatically complete giveaway tasks
+// @author         HCLonely
+// @license        MIT
+// @iconURL        https://github.com/HCLonely/auto-task/raw/master/favicon.ico
+// @homepage       https://blog.hclonely.com/posts/777c60d5/
+// @supportURL     https://github.com/HCLonely/auto-task/issues/new/choose
+// @updateURL      https://github.com/HCLonely/auto-task/raw/master/auto-task.user.js
+// @include        *://giveaway.su/giveaway/view/*
+// @include        *://marvelousga.com/*
+// @include        *://dupedornot.com/*
+// @include        *://www.grabfreegame.com/giveaway/*
+// @include        *://www.bananagiveaway.com/giveaway/*
+// @include        *://gamecode.win/*
+// @include        /https?:\/\/gamehag.com/.*?giveaway\/.*/
+// @include        *://prys.revadike.com/giveaway/?id=*
+// @include        *://www.indiedb.com/giveaways*
+// @include        *://www.opiumpulses.com/giveaways
+// @include        *://gkey.fun/distribution/*
+// @include        *://givekey.ru/distribution/*
+// @include        *://takekey.ru/distribution/*
+// @include        *://chubkeys.com/giveaway/*
+// @include        *://giveawayhopper.com/giveaway/*
+// @include        *://*freegamelottery.com*
+// @include        *://gleam.io/*
+// @include        *://www.spoune.com/index.php*
+// @exclude        *googleads*
+// @include        https://blog.hclonely.com/auto-task/setting.html
+// @include        https://blog.hclonely.com/auto-task/announcement.html
+// @require        https://cdn.bootcss.com/vue/2.6.10/vue.min.js
+// @require        https://cdn.bootcss.com/element-ui/2.12.0/index.js
+// @require        https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
+// @resource       css https://github.com/HCLonely/auto-task/raw/master/auto-task.min.css?ver=2.1.17
+// @grant          GM_setValue
+// @grant          GM_getValue
+// @grant          GM_listValues
+// @grant          GM_deleteValue
+// @grant          GM_addStyle
+// @grant          GM_xmlhttpRequest
+// @grant          GM_getResourceText
+// @grant          GM_registerMenuCommand
+// @grant          GM_info
+// @grant          GM_openInTab
+// @grant          GM_download
+// @connect        *
+// @run-at         document-end
+// @compatible     chrome >=58,没有测试其他浏览器的兼容性
 // ==/UserScript==
 
 (function() {

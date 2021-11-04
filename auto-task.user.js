2e06730c89e060520e0ff0da308dca5cf539b9c7
HCLonely <h1606051253@gmail.com>
2021-11-04 10:09:55 +0800

    修复twitter凭证更新失败的bug(#50)

--- auto-task.user.js
+++ auto-task.user.js
@@ -3,7 +3,7 @@
 // @name:en            Auto Task
 // @name:zh-CN         自动任务
 // @namespace          auto-task
-// @version            3.6.10
+// @version            3.6.11
 // @description        自动完成赠key站任务
 // @description:en     Automatically complete giveaway tasks
 // @description:zh-CN  自动完成赠key站任务
@@ -37,8 +37,8 @@
 // @include            https://auto-task.hclonely.com/setting.html
 // @include            https://auto-task.hclonely.com/notice-list.html
 
-// @require            https://cdn.jsdelivr.net/gh/HCLonely/auto-task@3.6.10/require/require.min.js
-// @resource           CSS https://cdn.jsdelivr.net/gh/HCLonely/auto-task@3.6.10/require/fuck-task.min.css
+// @require            https://cdn.jsdelivr.net/gh/HCLonely/auto-task@3.6.11/require/require.min.js
+// @resource           CSS https://cdn.jsdelivr.net/gh/HCLonely/auto-task@3.6.11/require/fuck-task.min.css
 
 // @grant              GM_setValue
 // @grant              GM_getValue

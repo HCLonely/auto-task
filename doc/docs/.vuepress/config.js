8e04976bc3b37ead84f3002c771acafed1b9e2cc
HCLonely <h1606051253@gmail.com>
2026-05-28 14:17:26 +0800

    - 优化：更新部分过时依赖

--- doc/docs/.vuepress/config.js
+++ doc/docs/.vuepress/config.js
@@ -1,65 +1,62 @@
-module.exports = {
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
+
+export default {
+  bundler: viteBundler(),
   locales: {
     '/': {
       lang: 'zh-CN',
       title: 'Auto Task',
-      description: '自动完成赠key站任务'
+      description: '自动完成赠key站任务',
     },
     '/en/': {
       lang: 'en-US',
       title: 'Auto Task',
-      description: 'Automatically complete giveaway tasks'
-    }
+      description: 'Automatically complete giveaway tasks',
+    },
   },
-  themeConfig: {
+  theme: defaultTheme({
     sidebarDepth: 2,
     sidebar: 'auto',
-    displayAllHeaders: true,
     lastUpdated: true,
-    themeConfig: {
-      nextLinks: false,
-      prevLinks: false
-    },
-    smoothScroll: true,
     repo: 'HCLonely/auto-task',
     repoLabel: 'Github',
     docsRepo: 'HCLonely/auto-task-doc',
     docsDir: 'docs',
-    editLinks: true,
-    activeHeaderLinks: true,
+    docsBranch: 'main',
+    editLink: true,
     locales: {
       '/': {
-        label: '简体中文',
-        selectText: 'Languages',
-        ariaLabel: 'Languages',
+        selectLanguageName: '简体中文',
+        selectLanguageText: 'Languages',
+        selectLanguageAriaLabel: 'Languages',
         editLinkText: '在 GitHub 上编辑此页',
-        lastUpdated: '上次更新',
-        nav: [
+        lastUpdatedText: '上次更新',
+        navbar: [
           { text: '首页', link: '/' },
           { text: '指南', link: '/guide/' },
           { text: '常见问题', link: '/FAQ/' },
           { text: '反馈', link: '/feedback/' },
           { text: '参与开发', link: '/dev/' },
           { text: '更新日志', link: '/logs/' },
-          { text: '其他脚本', link: '/other/' }
-        ]
+          { text: '其他脚本', link: '/other/' },
+        ],
       },
       '/en/': {
-        label: 'English',
-        selectText: '更改语言',
-        ariaLabel: '更改语言',
+        selectLanguageName: 'English',
+        selectLanguageText: '更改语言',
+        selectLanguageAriaLabel: '更改语言',
         editLinkText: 'Edit this page on GitHub',
-        lastUpdated: 'Last Updated',
-        nav: [
+        lastUpdatedText: 'Last Updated',
+        navbar: [
           { text: 'Home', link: '/en/' },
           { text: 'Guide', link: '/en/guide/' },
           { text: 'Q&A', link: '/en/FAQ/' },
           { text: 'Feedback', link: '/en/feedback/' },
           { text: 'Contribute', link: '/en/dev/' },
-          { text: 'Logs', link: '/en/logs/' }
-        ]
-      }
-    }
-  },
-  plugins: ['@vuepress/back-to-top']
+          { text: 'Logs', link: '/en/logs/' },
+        ],
+      },
+    },
+  }),
 }
